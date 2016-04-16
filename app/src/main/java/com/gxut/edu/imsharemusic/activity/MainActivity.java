6e50fd996a85703215c7abf32a64a6c5e9b5ef66
zzzzzhy <azure.and.sea@gmail.com>
2016-04-16 17:36:07 +0800

    测试

---
+++ app/src/main/java/com/gxut/edu/imsharemusic/activity/MainActivity.java
@@ -0,0 +1,44 @@
+package com.gxut.edu.imsharemusic.activity;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.gxut.edu.imsharemusic.R;
+import com.gxut.edu.imsharemusic.adapter.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setCurrentItem(1);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/gxut/edu/imsharemusic/adapter/SectionsPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.gxut.edu.imsharemusic.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.gxut.edu.imsharemusic.view.PlaceholderFragment;
+
+/**
+ * Created by Taste on 2016/4/9.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "SECTION 1";
+            case 1:
+                return "SECTION 2";
+            case 2:
+                return "SECTION 3";
+        }
+        return null;
+    }
+}
+
